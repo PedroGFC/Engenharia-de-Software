b76ee8d534a008ad54c8db0c036f294a5abc2a6b
joaomota <125226971+jov1ctor@users.noreply.github.com>
2025-10-02 23:30:26 -0300

    Update CandidaturaTest.java

--- src/test/java/com/mackenzie/eleicao/CandidaturaTest.java
+++ src/test/java/com/mackenzie/eleicao/CandidaturaTest.java
@@ -11,24 +11,28 @@ class CandidaturaTest {
 
     @Test
     void deveAprovar_quandoGostaDeAnimais_eMaiorQue18() {
+        System.out.println("Teste de parametros com sucesso");
         boolean resultado = Candidatura.aprovada(true, 19);
         assertEquals(true, resultado, "Esperava aprovação para idade > 18 e gosta de animais.");
     }
 
     @Test
     void naoDeveAprovar_quandoNaoGostaDeAnimais_mesmoSendoMaiorQue18() {
+        System.out.println("Teste de falha pois nao gosta de animais");
         boolean resultado = Candidatura.aprovada(false, 25);
         assertEquals(false, resultado, "Não deve aprovar se não gosta de animais.");
     }
 
     @Test
     void naoDeveAprovar_quandoGostaDeAnimais_masTem18() {
+        System.out.println("Teste de Idade = 18 anos, nao aprova");
         boolean resultado = Candidatura.aprovada(true, 18);
         assertEquals(false, resultado, "Idade precisa ser estritamente maior que 18.");
     }
 
     @Test
     void naoDeveAprovar_quandoGostaDeAnimais_masMenorDeIdade() {
+        System.out.println("Menor de idade. Nao aprova");
         boolean resultado = Candidatura.aprovada(true, 17);
         assertEquals(false, resultado, "Menor de idade não deve ser aprovado.");
     }
